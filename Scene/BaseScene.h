2748a731e96c51c1ac016c1f44e430a0086223fa
makonogh <ashegaruto@gmail.com>
2020-08-31 13:49:12 +0900

    +654641651

---
+++ Scene/BaseScene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <memory>
+class BaseScene;
+
+using unique_Base = std::unique_ptr<BaseScene>;
+
+class BaseScene
+{
+public:
+	BaseScene();
+	virtual ~BaseScene();
+	virtual unique_Base Update(unique_Base) = 0;
+	int SceneCount;
+};
---
+++ Scene/GameScene.cpp
@@ -0,0 +1,14 @@
+#include "GameScene.h"
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+}
+
+unique_Base GameScene::Update(unique_Base own)
+{
+	return unique_Base();
+}
